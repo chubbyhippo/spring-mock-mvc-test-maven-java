9995b96958bef9ef4806f11ee41e5c0995c27cc0
ch <mkmsacc@outlook.com>
2022-03-10 21:50:53 +0700

    Add test

---
+++ src/test/java/com/example/demo/HelloControllerTest.java
@@ -0,0 +1,37 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest
+class HelloControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private HelloService helloService;
+
+    @BeforeEach
+    void setup() {
+        Mockito.when(helloService.greet()).thenReturn("Hello");
+    }
+
+    @Test
+    void shouldReturnHello() throws Exception {
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("Hello")));
+    }
+
+}
