6a546dc00d7da7e8f19831558a4462bb501ee740
ch <mkmsacc@outlook.com>
2022-03-10 21:52:07 +0700

    Add Service

---
+++ src/main/java/com/example/demo/HelloController.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    private final HelloService helloService;
+
+    public HelloController(HelloService helloService) {
+        this.helloService = helloService;
+    }
+
+    @GetMapping
+    public String greet() {
+        return helloService.greet();
+    }
+}
