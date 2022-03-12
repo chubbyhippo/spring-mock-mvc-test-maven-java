0b98814e65bdafb8ce0079a01b36a56ae8a44292
ch <mkmsacc@outlook.com>
2022-03-12 10:31:53 +0700

    Add WebTestClient Test

--- src/test/java/com/example/demo/HelloControllerTest.java
+++ src/test/java/com/example/demo/HelloControllerTest.java
@@ -1,10 +1,12 @@
 package com.example.demo;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.reactive.server.WebTestClient;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.Matchers.containsString;
@@ -18,6 +20,9 @@ class HelloControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
+
+    @Autowired
+    private WebTestClient webTestClient;
 
     @MockBean
     private HelloService helloService;
@@ -34,4 +39,16 @@ class HelloControllerTest {
                 .andExpect(content().string(containsString("Hello")));
     }
 
+    @Test
+    void shouldReturnHelloUsingWebTestClient() {
+
+        String responseBody = webTestClient.get()
+                .uri("/")
+                .exchange()
+                .expectBody(String.class)
+                .returnResult()
+                .getResponseBody();
+
+        Assertions.assertThat(responseBody).isEqualTo("Hello");
+    }
 }
