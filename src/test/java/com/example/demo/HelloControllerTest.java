ea3b7c75ce47ec9c9281856579485c2be509c29d
ch <mkmsacc@outlook.com>
2022-03-12 10:32:33 +0700

    Add on-demand static

--- src/test/java/com/example/demo/HelloControllerTest.java
+++ src/test/java/com/example/demo/HelloControllerTest.java
@@ -1,6 +1,5 @@
 package com.example.demo;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +8,7 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.Matchers.containsString;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -49,6 +49,6 @@ class HelloControllerTest {
                 .returnResult()
                 .getResponseBody();
 
-        Assertions.assertThat(responseBody).isEqualTo("Hello");
+        assertThat(responseBody).isEqualTo("Hello");
     }
 }
